cf8e77feeea7488eb919a328e0cfdefd29e97e5c
Anthony Mensah Addae <maaddae.co@gmail.com>
2022-05-01 08:41:47 +0000

    Create diagnosis model viewset

--- src/crud/views.py
+++ src/crud/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+
+from crud.models import Diagnosis
+from crud.serializers import DiagnosisModelSerializer
+
+
+
+
+class DiagnosisModelViewSet(viewsets.ModelViewSet):
+    queryset = Diagnosis.objects.all()
+    serializer_class = DiagnosisModelSerializer
+    permission_classes = [permissions.AllowAny]
