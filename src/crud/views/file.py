a51eeec7a094a4b987865ff0f24a8992a0530c4b
Anthony Mensah Addae <maaddae.co@gmail.com>
2022-05-02 11:44:25 +0000

    Modify upload file view

--- src/crud/views/file.py
+++ src/crud/views/file.py
@@ -1,3 +1,4 @@
+from cgi import print_directory
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
@@ -6,7 +7,7 @@ from rest_framework.parsers import MultiPartParser, FormParser
 
 
 from crud.serializers.file import FileUploadSerializer
-from crud.utils.importer import csv_to_db
+from crud.tasks import csv_to_db_task
 
 
 
@@ -16,13 +17,8 @@ class UploadFileAPIView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
     
     def post(self, request, *args, **kwargs):
-        request.upload_handlers.pop(0)
-        file = self._file_upload(request,  *args, **kwargs)
-        csv_to_db(file)
-        return Response(status=status.HTTP_201_CREATED)
-    
-    
-    def _file_upload(self, request,  *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        return serializer.validated_data['file']
+        obj = serializer.save()
+        csv_to_db_task.delay(obj.pk)
+        return Response(status=status.HTTP_201_CREATED)
