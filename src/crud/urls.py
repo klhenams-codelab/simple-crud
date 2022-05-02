5d36383bd4518d4c1acf6cbec7e6824bc66b24d9
Anthony Mensah Addae <maaddae.co@gmail.com>
2022-05-02 03:57:58 +0000

    Add file upload url pattern

--- src/crud/urls.py
+++ src/crud/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from crud import views
+from crud.views import diagnosis
+from crud.views import file
 
 app_name='crud'
 router = DefaultRouter()
-router.register('diagnoses', views.DiagnosisModelViewSet, basename='diagnoses')
+router.register('diagnoses', diagnosis.DiagnosisModelViewSet, basename='diagnoses')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('upload/', file.UploadFileAPIView.as_view(), name='upload-file'),
 ]
