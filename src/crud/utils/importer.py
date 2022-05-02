b56578cbd83afb90a8416cb21a1a83e1c2f54d83
Anthony Mensah Addae <maaddae.co@gmail.com>
2022-05-02 03:53:29 +0000

    Add csv import utility

---
+++ src/crud/utils/importer.py
@@ -0,0 +1,27 @@
+import csv
+from crud.models import Diagnosis
+from rest_framework.exceptions import APIException
+
+
+def csv_to_db(file: str):
+
+    with open(file.temporary_file_path(), newline='') as f:
+        reader = csv.reader(f)
+        diagnosis_code_list = list(reader)
+        objs = [
+            Diagnosis(
+                category_code=diagnosis_code[0],
+                diagnosis_code=diagnosis_code[1],
+                abbreviated_description=diagnosis_code[3],
+                full_description=diagnosis_code[4],
+                category_title=diagnosis_code[5]
+            )
+            for diagnosis_code in diagnosis_code_list
+        ]
+
+    # print(objs)
+    try:
+        Diagnosis.objects.bulk_create(objs, ignore_conflicts=True)
+    except Exception as err:
+        # Pass to logger or something
+        raise APIException(f'Error: {err}')
